b5c3d2c95fc69961fda3cabf8a0c477718533fcf
Venkatesh Ramanujam <venki1092@gmail.com>
2016-09-18 21:11:13 -0700

    Update Client.java

--- Refresher__Java/src/MultiThreading/Client.java
+++ Refresher__Java/src/MultiThreading/Client.java
@@ -13,24 +13,11 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
-/**
- * A simple Swing-based client for the capitalization server.
- * It has a main frame window with a text field for entering
- * strings and a textarea to see the results of capitalizing
- * them.
- */
 public class Client {
 
     private BufferedReader in;
     private PrintWriter out;
     private String serverAddress = "localhost";
-    /**
-     * Constructs the client by laying out the GUI and registering a
-     * listener with the textfield so that pressing Enter in the
-     * listener sends the textfield contents to the server.
-     */
-    
     public Client() {
 
        
@@ -43,14 +30,6 @@ public class Client {
 	public void setserverAddress(String serveraddress) {
 		this.serverAddress = serveraddress;
 	}
-
-	/**
-     * Implements the connection logic by prompting the end user for
-     * the server's IP address, connecting, setting up streams, and
-     * consuming the welcome messages from the server.  The Capitalizer
-     * protocol says that the server sends three lines of text to the
-     * client immediately after establishing a connection.
-     */
     public void connectToServer() throws IOException {
 
        // Make connection and initialize streams
@@ -59,10 +38,6 @@ public class Client {
                 new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
     }
-
-    /**
-     * Runs the client application.
-     */
     public void initClient() throws Exception {
         
          connectToServer();
